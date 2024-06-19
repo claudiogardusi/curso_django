e52f2350ecc096fbe89bdb54f6a225ce1ea98816
claudiogadusi <claudio.gardusi@hotmail.com>
2024-06-19 14:24:32 -0300

    Create settings.json
    Criado novo arquivo settings

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+
+def my_view(request):
+    return HttpResponse('HOME')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', my_view), # Home
+    path('sobre/', my_view), # Sobre
+    path('contato/', my_view), # Contato
 ]
