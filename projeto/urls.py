d62adb66be80bc023783872cbbcd510bb5bff7cb
claudiogardusi <claudio.gardusi@hotmail.com>
2025-02-11 12:43:40 -0300

    Update urls.py
    Atualizado para estudo

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,18 +1,18 @@
 """
-URL configuration for projeto project.
+Configuração de URL para projeto projeto.(Tradução para lembrar!)
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+A lista `urlpatterns` roteia URLs para visualizações. Para mais informações, consulte:
+	https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Exemplos:
+Função visualizações
+	1. Adicione uma importação: from my_app import views
+	2. Adicione uma URL para urlpatterns: path('', views.home, name='home')
+Visualizações baseadas em classe
+	1. Adicione uma importação: from other_app.views import Home
+	2. Adicione uma URL para urlpatterns: path('', Home.as_view(), name='home')
+Incluindo outro URLconf
+	1. Importe a função include(): from django.urls import include, path
+	2. Adicione uma URL para urlpatterns: path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.http import HttpResponse
